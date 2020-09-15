e4b68d087651a88472828f54aa25eb306af0c208
N0tssss <nutssss@qq.com>
2020-09-15 11:13:42 +0800

    侧栏简介完成

--- js/N0ts.js
+++ js/N0ts.js
@@ -2,9 +2,13 @@
 function MenuClick_Open() {
     var menu = document.getElementById("leftBar-Menu");
     menu.classList.add("leftBar-Menu-Click");
+    var pages = document.getElementById("pages");
+    pages.classList.add("blur");
 }
 
 function MenuClick_Close() {
     var menu = document.getElementById("leftBar-Menu");
     menu.classList.remove("leftBar-Menu-Click");
+    var pages = document.getElementById("pages");
+    pages.classList.remove("blur");
 }
